8a915f09d1b9a51d1be27a5803a6377cb2907214
Kaique <kaiquedecampos2@gmail.com>
2023-11-14 17:36:37 -0300

    :recycle: refactor: refactor pastes

--- rocketseat/ignite-new/reactjs/ignite-feed/codes/src/components/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './Avatar.module.css';
-// eslint-disable-next-line react/prop-types
-export function Avatar({hasBorder = true, src}) {
-  return (
-    <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-    src={src} />
-  )
-}
